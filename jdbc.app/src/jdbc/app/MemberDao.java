f84b7d6af55aaff2c592b392183767a71c310702
Administrator <Administrator@smart000>
2017-05-31 14:29:54 +0900

    dbProperties 메서드 추가 selectMemberById 메서드 구성

--- jdbc.app/src/jdbc/app/MemberDao.java
+++ jdbc.app/src/jdbc/app/MemberDao.java
@@ -1,8 +1,51 @@
 package jdbc.app;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
 public class MemberDao {
-	public Member selectMemberById(String id) {
+	private String url;
+	private String dbid;
+	private String dbpw;
+	
+	private void dbPropertiesInit() throws IOException {
+		FileInputStream fis = new FileInputStream("d:\\db.properties");
+		Properties pro = new Properties();	// ?=?
+		pro.load(fis);
+		this.url = pro.getProperty(url);
+		this.dbid = pro.getProperty(dbid);
+		this.dbpw = pro.getProperty(dbpw);
+	}
+	
+	public Member selectMemberById(String id) throws ClassNotFoundException, SQLException, IOException {
+		this.dbPropertiesInit();
+		System.out.println(this.url);
+		System.out.println(this.dbid);
+		System.out.println(this.dbpw);
 		// properties db정보 가져옵니다.(내부적으로 input...
-		return null;
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection conn = DriverManager.getConnection(this.url,this.dbid,this.dbpw); 
+		String sql = "SELECT * FROM oracle_member WHERE ora_id=?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1, id);
+		ResultSet rs = stmt.executeQuery();
+		Member member = null;
+		if(rs.next()) {
+			member = new Member();
+			member.setId(rs.getString("ora_id"));
+			member.setPw(rs.getString("ora_pw"));
+			member.setLevel(rs.getString("ora_level"));
+			member.setName(rs.getString("ora_name"));
+			member.setEmail(rs.getString("ora_email"));
+		}
+		return member;
+	}
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+		MemberDao md = new MemberDao();
+		Member m = md.selectMemberById("id001");
+		System.out.println(m.getName().equals("홍길동"));
 	}
 }
